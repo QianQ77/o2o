06caa35cba46fdc076422b28214922ef576de062
QianQ77 <qianqiucs@gmail.com>
2019-09-21 19:03:09 -0400

    add Utils for generating thumbnails image

---
+++ src/main/java/com/qian/o2o/dto/ImageHolder.java
@@ -0,0 +1,35 @@
+package com.qian.o2o.dto;
+
+/**
+ * Created by qiuqian on 9/21/19.
+ */
+import java.io.InputStream;
+
+public class ImageHolder {
+
+    private String imageName;
+    private InputStream image;
+
+    public ImageHolder(String imageName, InputStream image) {
+        this.imageName = imageName;
+        this.image = image;
+    }
+
+    public String getImageName() {
+        return imageName;
+    }
+
+    public void setImageName(String imageName) {
+        this.imageName = imageName;
+    }
+
+    public InputStream getImage() {
+        return image;
+    }
+
+    public void setImage(InputStream image) {
+        this.image = image;
+    }
+
+}
+
---
+++ src/main/java/com/qian/o2o/utils/ImageUtil.java
@@ -0,0 +1,167 @@
+package com.qian.o2o.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.Random;
+
+import javax.imageio.ImageIO;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
+
+import com.qian.o2o.dto.ImageHolder;
+
+import net.coobird.thumbnailator.Thumbnails;
+import net.coobird.thumbnailator.geometry.Positions;
+
+/**
+ * Created by qiuqian on 9/21/19.
+ */
+public class ImageUtil {
+    private static String basePath = Thread.currentThread().getContextClassLoader().getResource("").getPath();
+    private static final SimpleDateFormat sDateFormat = new SimpleDateFormat("yyyyMMddHHmmss");
+    private static final Random r = new Random();
+    private static Logger logger = LoggerFactory.getLogger(ImageUtil.class);
+
+    /**
+     * Convert CommonsMultipartFile to File
+     *
+     * @param cFile
+     * @return
+     */
+    public static File transferCommonsMultipartFileToFile(CommonsMultipartFile cFile) {
+        File newFile = new File(cFile.getOriginalFilename());
+        try {
+            cFile.transferTo(newFile);
+        } catch (IllegalStateException e) {
+            logger.error(e.toString());
+            e.printStackTrace();
+        } catch (IOException e) {
+            logger.error(e.toString());
+            e.printStackTrace();
+        }
+        return newFile;
+    }
+
+    /**
+     * generate Thumbnail for input image to target path, and return its relative path
+     *
+     * @param thumbnail
+     * @param targetAddr
+     * @return
+     */
+    public static String generateThumbnail(ImageHolder thumbnail, String targetAddr) {
+        String realFileName = getRandomFileName();
+
+        String extension = getFileExtension(thumbnail.getImageName());
+
+        makeDirPath(targetAddr);
+
+        String relativeAddr = targetAddr + realFileName + extension;
+        logger.debug("current relativeAddr is :" + relativeAddr);
+
+        File dest = new File(PathUtil.getImgBasePath() + relativeAddr);
+        logger.debug("current complete addr is :" + PathUtil.getImgBasePath() + relativeAddr);
+        logger.debug("basePath is :" + basePath);
+        // generate thumbnail
+        try {
+            Thumbnails.of(thumbnail.getImage()).size(200, 200)
+                    .watermark(Positions.BOTTOM_RIGHT, ImageIO.read(new File(basePath + "/watermark.jpg")), 0.25f)
+                    .outputQuality(0.8f).toFile(dest);
+        } catch (IOException e) {
+            logger.error(e.toString());
+            throw new RuntimeException("Fail to generate thumbnail：" + e.toString());
+        }
+
+        return relativeAddr;
+    }
+
+    /**
+     * generate normal image
+     *
+     * @param thumbnail
+     * @param targetAddr
+     * @return
+     */
+    public static String generateNormalImg(ImageHolder thumbnail, String targetAddr) {
+        String realFileName = getRandomFileName();
+
+        String extension = getFileExtension(thumbnail.getImageName());
+
+        makeDirPath(targetAddr);
+
+        String relativeAddr = targetAddr + realFileName + extension;
+        logger.debug("current relativeAddr is :" + relativeAddr);
+
+        File dest = new File(PathUtil.getImgBasePath() + relativeAddr);
+        logger.debug("current complete addr is :" + PathUtil.getImgBasePath() + relativeAddr);
+        try {
+            Thumbnails.of(thumbnail.getImage()).size(337, 640)
+                    .watermark(Positions.BOTTOM_RIGHT, ImageIO.read(new File(basePath + "/watermark.jpg")), 0.25f)
+                    .outputQuality(0.9f).toFile(dest);
+        } catch (IOException e) {
+            logger.error(e.toString());
+            throw new RuntimeException("Fail to generate normal image：" + e.toString());
+        }
+        return relativeAddr;
+    }
+
+
+    /**
+     * make parent path if doesn't exist
+     * @param targetAddr
+     */
+    private static void makeDirPath(String targetAddr) {
+        String realFileParentPath = PathUtil.getImgBasePath() + targetAddr;
+        File dirPath = new File(realFileParentPath);
+        dirPath.mkdirs();
+    }
+
+    /**
+     * get Extension name
+     *
+     * @param fileName
+     * @return
+     */
+    private static String getFileExtension(String fileName) {
+        return fileName.substring(fileName.lastIndexOf("."));
+    }
+
+    /**
+     * generate random file name
+     *
+     * @return
+     */
+    public static String getRandomFileName() {
+        // 获取随机的五位数
+        int rannum = r.nextInt(89999) + 10000;
+        String nowTimeStr = sDateFormat.format(new Date());
+        return nowTimeStr + rannum;
+    }
+
+    public static void main(String[] args) throws IOException {
+        Thumbnails.of(new File("/Users/baidu/work/image/xiaohuangren.jpg")).size(200, 200)
+                .watermark(Positions.BOTTOM_RIGHT, ImageIO.read(new File(basePath + "/watermark.jpg")), 0.25f)
+                .outputQuality(0.8f).toFile("/Users/baidu/work/image/xiaohuangrennew.jpg");
+    }
+
+    /**
+     * Delete files
+     *
+     * @param storePath
+     */
+    public static void deleteFileOrPath(String storePath) {
+        File fileOrPath = new File(PathUtil.getImgBasePath() + storePath);
+        if (fileOrPath.exists()) {
+            if (fileOrPath.isDirectory()) {
+                Arrays.stream(fileOrPath.listFiles()).forEach(File::delete);
+            }
+            fileOrPath.delete();
+        }
+    }
+}
+
